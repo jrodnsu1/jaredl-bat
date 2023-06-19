aaffe17962b0ff4d2588dd83ba4993698ce34690
jrodnsu1 <78809797+jrodnsu1@users.noreply.github.com>
2023-06-19 13:11:11 -0500

    Create MSTeamsInstall.ps1

---
+++ MSTeamsInstall.ps1
@@ -0,0 +1,30 @@
+#.DESCRIPTION'Installs Microsoft Teams in all user mode'#
+
+Param(
+$url,
+$TeamsPath = "c:\temp\teams.msi"
+)
+
+$url32 = 'https://aka.ms/teams32bitmsi'
+$url64 = 'https://aka.ms/teams64bitmsi'
+
+if(!$url){
+    if([Environment]::Is64BitOperatingSystem){
+        $url = $url64
+    }
+    else{
+        $url = $url32
+    }
+}
+
+$client = new-object System.Net.WebClient
+$client.DownloadFile($url,$TeamsPath) 
+
+$return = Start-Process msiexec.exe -Wait -ArgumentList "/I $TeamsPath /qn /norestart"  -PassThru
+
+if(@(0,3010) -contains $return.ExitCode){
+return 'Installed'
+}
+else{
+return 'Error Installing'
+}
